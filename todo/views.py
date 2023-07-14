18bded1721b9453633807d186fc2cd74627fdbe8
Samsul-Alam-STP <samsul.alam.stp@gmail.com>
2023-07-14 17:23:49 +0600

    adding new task in todo

---
+++ todo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import redirect, render
+from .models import Task
+
+
+def addTask(request):
+    # print(request.POST)
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
